cf375e82b31977f18164eed374e1fb254deff262
Mateo Ávila Pava <mateoap@datup.ai>
2022-11-27 17:10:14 +0100

    fill setup and requirements files

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="snowflake",
+    version="0.1",
+    author="Mateo Avila",
+    author_email="mateo.avila@fau.de",
+    description="Exercise 5 package",
+    packages=["snowflake"]
+)
